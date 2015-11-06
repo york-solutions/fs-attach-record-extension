2302eaf17571ac4e4c023cf181c9a1733bfaae9b
Justin <justincyork@gmail.com>
2015-11-06 14:38:47 -0600

    Basic working version

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('fs attach record');
+
+function loadResource(url) {
+  return new Promise(function(resolve, reject){
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'import');
+    link.setAttribute('href', url);
+    link.onload = function() {
+      resolve(url);
+    };
+    document.head.appendChild(link);
+  });
+}
+
+function setup(){
+  // Add fs-client
+  var fsClient = document.createElement('fs-client');
+  fsClient.setAttribute('client-id', 'a02j000000AhNBEAA3');
+  fsClient.setAttribute('redirect-uri', '/_fs-auth');
+  fsClient.setAttribute('environment', 'sandbox');
+  fsClient.setAttribute('save-access-token','');
+  document.body.appendChild(fsClient);
+  
+  // Add fs-attach-record
+  var attach = document.createElement('fs-attach-record');
+  attach.setAttribute('person-id', 'KWCF-GK5');
+  attach.setAttribute('url', window.location.href);
+  document.querySelector('#attach-to-tree').appendChild(attach);
+}
+
+loadResource(chrome.extension.getURL('components.html'))
+  .then(setup);
+  
